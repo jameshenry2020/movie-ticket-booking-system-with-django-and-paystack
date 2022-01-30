84eb1f8ef93ee1d5c64db8b7353112c54c0a0cfa
henry <aengrhenry@gmail.com>
2022-01-30 23:41:48 +0100

    added the new models to the admin

--- src/movies/admin.py
+++ src/movies/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .models import WatchDay, ShowTime, Movies
+from .models import WatchDay, ShowTime, Movies,TicketBooking,Audience
 # Register your models here.
 
 admin.site.register(WatchDay)
 admin.site.register(Movies)
 admin.site.register(ShowTime)
+admin.site.register(Audience)
+admin.site.register(TicketBooking)
